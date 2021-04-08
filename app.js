263a97caa175d14362ed5c5e8b953da76669940f
David <davidjhinku@gmail.com>
2021-04-08 14:34:23 -0400

    Connected mongoose to db

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const db = require('./config/keys').mongoURI;
 
-
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Mongo works"))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send("This works, huzzah!"))
 
